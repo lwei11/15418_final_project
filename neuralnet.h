1f0f818510cd46c2337860f49f5e2a2f861ffda5
lanyuntw@catshark <lanyuntw@andrew.cmu.edu>
2024-12-03 22:40:58 -0500

    working implementation of batch parallel

--- neuralnet.h
+++ neuralnet.h
@@ -27,22 +27,19 @@ public:
 
 // Sigmoid Activation Class
 class Sigmoid {
-private:
-    Vector z;  // Cache for backward pass
 public:
+    Vector z;  // Cache for backward pass
     Vector forward(const Vector& x);
     Vector backward(const Vector& dz);
 };
 
 // Linear Layer Class
 class Linear {
-private:
+public:
     Matrix weights;  // Weights and biases
     Matrix gradients;  // Gradients for backpropagation
     double learning_rate;
     Vector input_with_bias;  // Cache for forward pass
-
-public:
     Linear(int input_size, int output_size, Matrix (*init_fn)(int, int), double lr);
     Vector forward(const Vector& x);
     Vector backward(const Vector& dz);
@@ -51,13 +48,11 @@ public:
 
 // Neural Network Class
 class NN {
-private:
+public:
     Linear linear1;
     Sigmoid sigmoid;
     Linear linear2;
     SoftMaxCrossEntropy softmax;
-
-public:
     NN(int input_size, int hidden_size, int output_size, Matrix (*init_fn)(int, int), double lr);
     std::tuple<Vector, double> forward(const Vector& x, int y);
     void backward(int y, const Vector& y_hat);
@@ -66,7 +61,7 @@ public:
     std::tuple<std::vector<double>, std::vector<double>> train(
         const Matrix& X_train, const Vector& y_train,
         const Matrix& X_test, const Vector& y_test,
-        int epochs, int batch_size);
+        int epochs, int batch_size, int nproc, int pid);
     std::tuple<Vector, double> test(const Matrix& X, const Vector& y);
 };
 
